69e8b01aa75399f43e3596245fb0e98ffa2ea126
Gabriela <gabrielasap19@gmail.com>
2025-08-27 10:13:20 -0300

    fix `/migrations` endpoint connection bug

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,7 +6,7 @@ async function cleanDatabase() {
 }
 
 test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http://localhost:3001/api/v1/migrations", {
     method: "POST",
   });
   expect(response1.status).toBe(201);
@@ -15,7 +15,7 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
 
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response2 = await fetch("http://localhost:3001/api/v1/migrations", {
     method: "POST",
   });
   expect(response2.status).toBe(200);
